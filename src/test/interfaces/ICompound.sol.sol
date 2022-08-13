627915d1903af3e2817a2d12543b67b9765db128
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2022-08-13 16:42:59 +0800

    add ICompound.sol

---
+++ src/test/interfaces/ICompound.sol.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+interface CErc20 {
+  function balanceOf(address) external view returns (uint);
+
+  function mint(uint) external returns (uint);
+
+  function exchangeRateCurrent() external returns (uint);
+
+  function supplyRatePerBlock() external returns (uint);
+
+  function balanceOfUnderlying(address) external returns (uint);
+
+  function redeem(uint) external returns (uint);
+
+  function redeemUnderlying(uint) external returns (uint);
+
+  function borrow(uint) external returns (uint);
+
+  function borrowBalanceCurrent(address) external returns (uint);
+
+  function borrowRatePerBlock() external view returns (uint);
+
+  function repayBorrow(uint) external returns (uint);
+
+  function liquidateBorrow(
+    address borrower,
+    uint amount,
+    address collateral
+  ) external returns (uint);
+}
+
+interface CEth {
+  function balanceOf(address) external view returns (uint);
+
+  function mint() external payable;
+
+  function exchangeRateCurrent() external returns (uint);
+
+  function supplyRatePerBlock() external returns (uint);
+
+  function balanceOfUnderlying(address) external returns (uint);
+
+  function redeem(uint) external returns (uint);
+
+  function redeemUnderlying(uint) external returns (uint);
+
+  function borrow(uint) external returns (uint);
+
+  function borrowBalanceCurrent(address) external returns (uint);
+
+  function borrowRatePerBlock() external view returns (uint);
+
+  function repayBorrow() external payable;
+}
+
+interface Comptroller {
+  function markets(address)
+    external
+    view
+    returns (
+      bool,
+      uint,
+      bool
+    );
+
+  function enterMarkets(address[] calldata) external returns (uint[] memory);
+
+  function getAccountLiquidity(address)
+    external
+    view
+    returns (
+      uint,
+      uint,
+      uint
+    );
+
+  function closeFactorMantissa() external view returns (uint);
+
+  function liquidationIncentiveMantissa() external view returns (uint);
+
+  function liquidateCalculateSeizeTokens(
+    address cTokenBorrowed,
+    address cTokenCollateral,
+    uint actualRepayAmount
+  ) external view returns (uint, uint);
+}
+
+interface PriceFeed {
+  function getUnderlyingPrice(address cToken) external view returns (uint);
+}
