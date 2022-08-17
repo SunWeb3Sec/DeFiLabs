7446a6207f746bd8b936170fb96d03d1c747ed3d
eugenioclrc <eugenioclrc@gmail.com>
2022-08-16 23:04:23 -0300

    add console2 and label for tokens

--- src/test/Uniswapv2.sol
+++ src/test/Uniswapv2.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.10;
 
 import "forge-std/Test.sol";
+import "forge-std/console2.sol";
 import "./interfaces/IWETH9.sol";
 import "./interfaces/IUniswap.sol";
 import "./interfaces/IERC20.sol";
@@ -14,19 +15,31 @@ contract ContractTest is Test {
 
   function setUp() public {
     vm.createSelectFork("mainnet", 15327706); //fork mainnet at block 15012670
-    vm.startPrank(0x218B95BE3ed99141b0144Dba6cE88807c4AD7C09);
-    WBTC.transfer(address(this),100000000); // transfer 1 BTC to self-contract
 
+    vm.label(address(WBTC), "WBTC");
+    vm.label(address(DAI), "DAI");
+    vm.label(address(WETH), "WETH");
+    vm.label(address(UNISWAP_V2_ROUTER), "ROUTER");
+
+    address here = address(this);
+    vm.startPrank(0x218B95BE3ed99141b0144Dba6cE88807c4AD7C09);
+    WBTC.transfer(here, 1*(10**WBTC.decimals())); // transfer 1 BTC to self-contract
+    vm.stopPrank();
   }
  
   function testUniswapv2_swap() public {
+    uint256 daiToDecimals = 10**DAI.decimals();
+    uint256 wtbcToDecimals = 10**WBTC.decimals();
 
-    console.log("----Swap 1 WBTC to DAI----");
+    console2.log("----Swap 1 WBTC to DAI----");
+    console2.log("DAI balance before swap:", DAI.balanceOf(address(this))/daiToDecimals);
+    console2.log("WBTC balance before swap:", WBTC.balanceOf(address(this))/wtbcToDecimals);
+    console2.log("----");
     swap(address(WBTC),address(DAI),100000000,1,address(this));
-    emit log_named_uint("DAI balance:", DAI.balanceOf(address(this))/1e18);
-    emit log_named_uint("WBTC balance:", WBTC.balanceOf(address(this))/1e18);
+    console2.log("DAI balance after swap:", DAI.balanceOf(address(this))/daiToDecimals);
+    console2.log("WBTC balance after swap:", WBTC.balanceOf(address(this))/wtbcToDecimals);
   }
-   function swap(
+  function swap(
     address _tokenIn,
     address _tokenOut,
     uint _amountIn,
